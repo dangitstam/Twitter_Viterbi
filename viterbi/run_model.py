b44ea7c885ff039ba14acea50f1902c844ac34f0
Tam Dang <tamdangnadmat@gmail.com>
2020-05-17 17:09:45 -0700

    clean up

--- viterbi/run_model.py
+++ viterbi/run_model.py
@@ -9,32 +9,32 @@ def main():
     )
     args = parser.parse_args()
 
+    print("Loading model...")
     archived_model = pickle.load(open(args.model_path, "rb"))
     model = archived_model.get("model")
     dataset_reader = archived_model.get("dataset_reader")
 
-    interact = input("\nModel trained. Test with a few examples? (y/n): ")
-    if interact.lower() == "y":
-        try:
-            while True:
-                tweet = input("\nType an example tweet (press CTRL-C to exit): ")
-                tokens = tweet.split()
-                processed_tokens = dataset_reader.token_preprocessing_fn(tokens)
-                output = model.predict(processed_tokens)
-                prediction_labels = output["labels"]
-                print("Input: {}".format(tweet))
-                print("Output:")
-                print("Prediction: {}".format(" ".join(prediction_labels)))
-                print(
-                    "\n".join(
-                        [
-                            "Token: {:25s} Label: {:25s}".format(token, label)
-                            for token, label in zip(tokens, prediction_labels)
-                        ]
-                    )
+    print("Model successfully loaded.")
+    try:
+        while True:
+            tweet = input("\nType an example tweet (press CTRL-C to exit): ")
+            tokens = tweet.split()
+            processed_tokens = dataset_reader.token_preprocessing_fn(tokens)
+            output = model.predict(processed_tokens)
+            prediction_labels = output["labels"]
+            print("Input: {}".format(tweet))
+            print("Output:")
+            print("Prediction: {}".format(" ".join(prediction_labels)))
+            print(
+                "\n".join(
+                    [
+                        "Token: {:25s} Label: {:25s}".format(token, label)
+                        for token, label in zip(tokens, prediction_labels)
+                    ]
                 )
-        except KeyboardInterrupt:
-            pass
+            )
+    except KeyboardInterrupt:
+        print()
 
 
 if __name__ == "__main__":
